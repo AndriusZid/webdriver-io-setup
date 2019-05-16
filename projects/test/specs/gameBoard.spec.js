a153bab92d81658338ea240ab3e1ec62f15e5be7
GerdaMonkeviciute <gerda.monkeviciute@devbridge.com>
2019-05-16 13:51:03 +0300

    basic operations with local storage

--- projects/test/specs/gameBoard.spec.js
+++ projects/test/specs/gameBoard.spec.js
@@ -3,7 +3,18 @@ const { percySnapshot } = require('@percy/webdriverio')
 describe('Integration test with visual testing', () => {
     it('Loads the hame page and takes snapshot to compare with previous version site', () => {
       browser.url('https://4ark.me/2048/');
-      percySnapshot(browser, 'gameboard');
-      browser.pause(10000);
+      //percySnapshot(browser, 'gameboard');
+      //browser.pause(10000);
+      browser.pause(1000);
+
+      browser.keys("Up arrow");
+      browser.keys("Down arrow");
+      browser.pause(1000);
+
+      browser.localStorage('POST', {key: 'gameState', value: '{"cell":[{"val":4,"index":0},{"val":8,"index":1},{"val":2,"index":2},{"val":2,"index":3},{"val":2,"index":4},{"val":8,"index":5},{"val":32,"index":6},{"val":4,"index":7},{"val":0,"index":8},{"val":0,"index":9},{"val":0,"index":10},{"val":0,"index":11},{"val":0,"index":12},{"val":0,"index":13},{"val":2,"index":14},{"val":0,"index":15}],"socre":92}'});
+  
+      browser.pause(1000);  
+      browser.refresh();
+      browser.pause(1000); 
     })
   })
