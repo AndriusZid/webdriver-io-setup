86a3137b57d834a680ad7b9ef95906c332b50f61
Pijus Kamandulis <pikami@airmail.cc>
2019-05-16 15:29:23 +0300

    Display correct score text

--- projects/test/specs/validateText.spec.js
+++ projects/test/specs/validateText.spec.js
@@ -25,6 +25,16 @@ describe('Testing the static text in Game intro', () => {
         expect(newScore).toBe('0');
     });
 
+    it('Should display score from local storrage correctly', () => {
+        const resetButton = browser.$('.restart-btn');
+        browser.localStorage('POST', {key: 'gameState', value: '{"cell":[{"val":4,"index":0},{"val":8,"index":1},{"val":2,"index":2},{"val":2,"index":3},{"val":2,"index":4},{"val":8,"index":5},{"val":32,"index":6},{"val":4,"index":7},{"val":0,"index":8},{"val":0,"index":9},{"val":0,"index":10},{"val":0,"index":11},{"val":0,"index":12},{"val":0,"index":13},{"val":2,"index":14},{"val":0,"index":15}],"socre":92}'});
+        browser.refresh();
+
+        const score = browser.$('.score').getText();
+        expect(score).toBe('92');
+        resetButton.click(); // Reset
+    });
+
     //BEST CONTAINER
     it('Best container text is correct.', () => {
         const bestContainer = browser.$('.best-container').getText();
